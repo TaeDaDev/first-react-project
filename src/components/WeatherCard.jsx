2bed08fb23aa796622ae016328d5cc8a145bc9e2
TaeDaDev <uhsaantae@gmail.com>
2026-03-30 17:54:59 -0400

    added high and low, with tailwind

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -3,10 +3,26 @@ export default function WeatherCard(props) {
   if (!main || !wind || !weather) {
     return <p>Please enter a valid city name.</p>;
   }
+
   return (
     <div>
       <h2>{props.data.name}</h2>
-      {props.toggle ? <p>{Math.round(main.temp * 9/5 - 459.67)}°F</p> : <p>{Math.round(main.temp - 273.15)}°C</p>}
+      {props.toggle ? (
+        <p>{Math.round((main.temp * 9) / 5 - 459.67)}°F</p>
+      ) : (
+        <p>{Math.round(main.temp - 273.15)}°C</p>
+      )}
+      {props.toggle ? (
+        <>
+          <p>Low: {Math.round((main.temp_min * 9) / 5 - 459.67)}°F</p>
+          <p>High: {Math.round((main.temp_max * 9) / 5 - 459.67)}°F</p>
+        </>
+      ) : (
+        <>
+          <p>Low: {Math.round(main.temp_min - 273.15)}°C</p>
+          <p>High: {Math.round(main.temp_max - 273.15)}°C</p>
+        </>
+      )}
       <p>Humidity: {main.humidity}%</p>
       <p>Wind Speed: {wind.speed} m/s</p>
       <p>
@@ -19,5 +35,3 @@ export default function WeatherCard(props) {
     </div>
   );
 }
-
-
